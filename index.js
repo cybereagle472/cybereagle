f637068245bb064e5ae7cf7ce1044c987e67970c
cybereagle472 <usa66486@gmail.com>
2026-01-12 15:22:03 +0500

    Update index.js

--- index.js
+++ index.js
@@ -14,8 +14,8 @@ const pino = require("pino");
 
 // --- PERSONALIZATION & AI CORE ---
 const OWNER_NAME = "Muhammad Nasir";
-const MY_NUMBER = "923441675749";
-const SESSION_ID = "EagleX_Ultra_Pro_V3_Active"; 
+const MY_NUMBER = "9779822691613";
+const SESSION_ID = "EagleX_UltraPro_1"; 
 
 const AI_SYSTEM_PROMPT = `
 You are EagleX, the elite personal AI assistant and "Digital Twin" of ${OWNER_NAME}.
