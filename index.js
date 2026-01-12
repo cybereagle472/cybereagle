180b2a50fb14a5219c4eccb2aac6935f4102d997
cybereagle472 <usa66486@gmail.com>
2026-01-13 01:26:41 +0500

    Update index.js

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ async function startEagleX() {
     const { version } = await fetchLatestBaileysVersion();
     
     // Shared Session ID: Must match what you use in Termux pair.js
-    const { state, saveCreds } = await usePostgreSQLAuthState(pool, "eaglex_pro_max");
+    const { state, saveCreds } = await usePostgreSQLAuthState(pool, "eaglex_session");
     
     const sock = makeWASocket({
         version,
