3ee1e518371cf74b016d584f25efc396a8435028
cybereagle472 <usa66486@gmail.com>
2026-05-18 23:11:19 +0500

    Update main-cmd.js

--- plugins/main-cmd.js
+++ plugins/main-cmd.js
@@ -645,8 +645,8 @@ async (conn, mek, m, { from }) => {
     try {
         // Owner's contact info
         const ownerNumber = '+923245115847'; // Replace this with the actual owner number
-        const ownerName = 'Nasir 🎭`; // Replace this with the owner's name
-        const organization = 'CyberWorld` ; // Optional: replace with the owner's organization
+        const ownerName = 'Nasir 🎭'; // Replace this with the owner's name
+        const organization = 'CyberWorld'; // Optional: replace with the owner's organization
 
         // Create a vCard (contact card) for the owner
         const vcard = 'BEGIN:VCARD\n' +
